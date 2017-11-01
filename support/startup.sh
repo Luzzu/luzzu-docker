d95e5c6e97a7baea4dc79a560beb59cf77098b00
Jeremy Debattista <jerdebattista@gmail.com>
2017-11-01 21:51:49 +0000

    added support and tools

---
+++ support/startup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+apachectl start
+nohup mvn exec:java -X -f /usr/bin/Luzzu/luzzu-communications/pom.xml > /tmp/luzzu_output.log &
+
+/bin/echo "Available commands: cancel failed pending stats status success"
+/bin/bash
