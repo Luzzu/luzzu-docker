921fb766c141ceb36b453f0ac360c5a9921b2fbf
Jeremy Debattista <jerdebattista@gmail.com>
2019-01-19 17:57:41 +0000

    update

--- support/docker.sh
+++ support/docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PATH_INSTALLATION='/usr/bin/' # the path where Luzzu and the Quality Metric repository should be installed
+PATH_INSTALLATION='/usr/bin' # the path where Luzzu and the Quality Metric repository should be installed
 
 # Pull and build RDFHDT
 git clone https://github.com/rdfhdt/hdt-java.git
@@ -11,12 +11,12 @@ mvn clean install
 echo "Installing Luzzu in $PATH_INSTALLATION"
 cd $PATH_INSTALLATION
 echo "Pullingand Installing Luzzu"
-git clone https://github.com/Luzzu/Framework.git Luzzu
-cd Luzzu
+git clone https://github.com/Luzzu/Framework.git
+cd Framework
 mvn clean install
 
 echo "Setting up Metrics"
-cd $PATH_INSTALLATION/Luzzu/luzzu-communications/externals/metrics/
+cd $PATH_INSTALLATION/Framework/luzzu-communications/externals/metrics/
 wget http://s001.adaptcentre.ie/FrameworkMetrics/LDMetrics/intrinsic.zip
 unzip intrinsic.zip
 rm intrinsic.zip
@@ -27,7 +27,7 @@ wget http://s001.adaptcentre.ie/FrameworkMetrics/LDMetrics/contextual.zip
 unzip contextual.zip
 rm contextual.zip
 
-cd $PATH_INSTALLATION/Luzzu/luzzu-communications/externals/vocabs/
+cd $PATH_INSTALLATION/Framework/luzzu-communications/externals/vocabs/
 wget http://s001.adaptcentre.ie/FrameworkMetrics/Vocabs/dqm.zip
 unzip dqm.zip
 rm dqm.zip
