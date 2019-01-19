078ddb20ed3ba64a20c67bf7c77d6dd5de5b0431
Jeremy Debattista <jerdebattista@gmail.com>
2019-01-19 16:41:58 +0000

    updated tools for v4

--- support/startup.sh
+++ support/startup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-apachectl start
 nohup mvn exec:java -X -f /usr/bin/Luzzu/luzzu-communications/pom.xml > /tmp/luzzu_output.log &
 
 /bin/echo "Available commands: cancel failed pending stats status success"
