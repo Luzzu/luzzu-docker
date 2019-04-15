d7600d590119b21532209ea3ff8a59b91ea4c576
Jeremy Debattista <jerdebattista@gmail.com>
2019-04-15 15:19:38 +0100

    Update startup.sh

--- support/startup.sh
+++ support/startup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-cd /Luzzu/Framework/luzzu-communications/
-mvn exec:java > /tmp/framework_output.log 
+# cd /Luzzu/Framework/luzzu-communications/
+# mvn exec:java > /tmp/framework_output.log 
+
+nohup mvn exec:java -X -f /usr/bin/Framework/luzzu-communications/pom.xml > /tmp/luzzu_output.log &
