9b04e5216317a5a45b2005bbd9254a21529ddf9c
Jeremy Debattista <jerdebattista@gmail.com>
2019-04-15 14:38:33 +0100

    Update startup.sh

--- support/startup.sh
+++ support/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-nohup mvn exec:java -X -f /usr/bin/Framework/luzzu-communications/pom.xml > /tmp/luzzu_output.log &
+nohup mvn exec:java -X -f /Luzzu/Framework/luzzu-communications/pom.xml > /tmp/framework_output.log &
 
 /bin/echo "Available commands: cancel failed pending stats status success"
 /bin/bash
